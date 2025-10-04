f3007d78baef169f76a962891ecbb29b4d692996
focus-wtv <lverbauwhede@rmm.be>
2025-10-04 23:16:58 +0200

    admin route

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === 'development' ? '/' : (process.env.VITE_BASE_PATH || '/reizenFocusWtv'),
+  base: '/',
 }));
