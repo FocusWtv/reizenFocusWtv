e3f08f6d4ca39d0b5178906799a9156c347c28ce
focus-wtv <lverbauwhede@rmm.be>
2026-05-07 11:47:02 +0200

    kleurtjes

--- src/admin/components/ReisSections.jsx
+++ src/admin/components/ReisSections.jsx
@@ -506,11 +506,12 @@ const ReisSections = ({
                     className="w-full border rounded px-3 py-2"
                   >
                     <option value="">Geen</option>
-                    <option value="bg-purple-200">Paars (licht)</option>
-                    <option value="bg-green-200">Groen (licht)</option>
-                    <option value="bg-orange-200">Oranje (licht)</option>
-                    <option value="bg-blue-200">Blauw (licht)</option>
-                    <option value="bg-gray-200">Grijs (licht)</option>
+                    <option value="bg-purple-300">Paars (licht)</option>
+                    <option value="bg-green-300">Groen (licht)</option>
+                    <option value="bg-orange-300">Oranje (licht)</option>
+                    <option value="bg-blue-300">Blauw (licht)</option>
+                    <option value="bg-gray-300">Grijs (licht)</option>
+                    <option value="bg-yellow-300">Geel (licht)</option>
                   </select>
                 </div>
                 <div className="md:col-span-3 flex items-center justify-between">
